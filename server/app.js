721f13bee0c805110804775a8e4eef1fde9ce109
Nathaniel Scott Bailey <nathanielscottbailey@Nathaniels-MacBook-Air.local>
2017-06-27 16:17:42 -0400

    added username

--- server/app.js
+++ server/app.js
@@ -1,11 +1,64 @@
 var express= require('express');
 var app= express();
- 
+var Sequelize = require('sequelize');
 app.use(require('./middleware/header'));
+//name of the data base, database type, and since its postgres the password I use for it
+var bodyParser= require('body-parser');
+
+
+var seq = new Sequelize('workoutlog', 'postgres', 'Rudy18nat',{
+	host:'localhost',
+	dialect: 'postgres'
+});
+
+
+
+seq.authenticate().then(
+	function(){
+		console.log("You're connected to the pg database")
+	},
+		function(err){
+			console.log(err)
+})
+
+
+var User= seq.define('user',{
+
+	username: Sequelize.STRING,
+	passwordhash: Sequelize.STRING
+})
+
+User.sync()
+app.use(bodyParser.json())
+
+app.post('/api/user', function(req,res){
+	var username= req.body.user.username
+	var pass= req.body.user.password
+
+
+
+User.create({
+	username: username,
+	passwordhash: ""
+	}).then(
+		function createSuccess(user){
+			res.json({
+				user: user,
+				message: 'create'
+			})
+		},
+		function createError(err){
+			res.send(500, err.message)
+		}
+
+		)
+})
 
 app.use('/api/test', function(req,res){
 	res.send("Hello World");
 });
+
+
 
 app.listen(3000,function(){
 	console.log("app is listening on 3000")
